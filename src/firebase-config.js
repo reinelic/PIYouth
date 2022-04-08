e449a2adc78834435857a8a8fb61b06a8bd14511
reinelic <reinelic@yahoo.fr>
2022-04-08 10:46:58 +0200

    Configuration de base firebase auth

---
+++ src/firebase-config.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.REACT_FIREBASE_AUTH_APIKEY,
+  authDomain: process.env.REACT_FIREBASE_AUTHDOMAIN,
+  projectId: process.env.REACT_FIREBASE_AUTH_APIKEY,
+  storageBucket: process.env.REACT_FIREBASE_AUTH_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_FIREBASE_AUTH_MESSAGINGSENDERID,
+  appId: process.env.REACT_FIREBASE_AUTH_APPID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
