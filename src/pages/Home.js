d05df4fc319c0d40f9382331be8d1cd10eed270d
reinelic <reinelic@yahoo.fr>
2025-05-06 14:49:40 +0200

    changing copy to make it Suma

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,7 +11,6 @@ import { UserContext } from '../context/userContext'
 
 const Home = () => {
   const [cds, setCds] = useState([])
-
   const { toggleModals, currentUser } = useContext(UserContext)
 
   useEffect(() => {
@@ -20,45 +19,38 @@ const Home = () => {
 
   const getCDS = async () => {
     const data = await axios.get('https://api.npoint.io/21d771eb6ff6e4e0fbe3')
-
-    console.log('This are the cds')
-
-    console.log(data.data.cds)
-
     setCds(data.data.cds)
   }
 
   return (
     <>
       <div className='container-fluid wrapper'>
-        <div className=' row home  flex '>
-          <div className=' col-md-7 home_info'>
-            <h1 className='home_info--titl e fw-bold fs-primary-heading text-primary-200'>
-              <span class='text-accent-heading '> Femmes Resilientes</span>{' '}
+        <div className='row home flex'>
+          <div className='col-md-7 home_info'>
+            <h1 className='home_info--title fw-bold fs-primary-heading text-primary-200'>
+              <span className='text-accent-heading'>SUMA</span>
               <br />
-              Un monde Resilient
+              Autonomisation Financière des Femmes
             </h1>
 
             <div className='home_info--body text-primary-200 fs-section'>
-              SUMA aide les femmes a avoir acces aux services financiers et a
-              les equiper avec les ressources pour faire face a leurs besoins.
-              Rejoignez la communaute des femmes SUMA
+              SUMA connecte les entrepreneures aux institutions financières,
+              leur fournit une éducation financière essentielle, et facilite
+              l'épargne et le développement de leurs entreprises tout en
+              renforçant leurs communautés.
             </div>
 
             <div className='home_info--action d-flex'>
               <div
-                className=' action_a bg-button box-shadow'
-                onClick={() => {
-                  console.log('clicking here ...')
-                  toggleModals('SignUp')
-                }}
+                className='action_a bg-button box-shadow'
+                onClick={() => toggleModals('SignUp')}
               >
-                Rejoignez la communaute{' '}
+                Rejoignez notre mouvement
               </div>
 
               <div className='action_b'>
                 <Link to='/cdsList'>
-                  <span className='mr-2'> CDS jeunes</span>{' '}
+                  <span className='mr-2'>Réseaux de femmes</span>
                   <Hospital size={48} />
                 </Link>
               </div>
@@ -69,24 +61,22 @@ const Home = () => {
         </div>
 
         <div className='container-fluid methodes padding-block-big'>
-          {/* <div className=" bg-danger position-relative top-0 w-25">
-
-<h4> Les Methodes de contraception</h4> 
-
-</div>     */}
-
-          <div className=' row bg-body-primary '>
+          <div className='row bg-body-primary'>
             <div className='col-md-7 contra'>
               <img
                 src='/contra_component.png'
-                alt=''
+                alt='Femmes entrepreneures'
                 className='component mb-2'
               />
-
-              <div className='text-primary-100 w-70 fs-section mt-4 '></div>
+              <div className='text-primary-100 w-70 fs-section mt-4'>
+                <h3>Notre Approche Holistique</h3>
+                <p>
+                  Nous combinons accès au financement, formation en gestion
+                  d'entreprise, et mentorat pour créer un écosystème complet de
+                  soutien aux femmes.
+                </p>
+              </div>
             </div>
-
-            {/* Methodes  */}
 
             <Methodes />
           </div>
@@ -94,33 +84,33 @@ const Home = () => {
           <Activites />
 
           {cds.data ? (
-            <div className=' row bg-dark p-4'>
+            <div className='row bg-dark p-4'>
               {cds.map((elt) => (
                 <div className='col-md-4'>
-                  {' '}
                   <Cds data={elt} />
                 </div>
               ))}
             </div>
-          ) : (
-            <div></div>
-          )}
+          ) : null}
 
           <div className='row border-primary mt-4 mb-4'>
             <div className='col-md-4 bg-primary cdsagent p-2'>
-              {/* <img src="https://images.unsplash.com/photo-1645378999013-95abebf5f3c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="" className="img" /> */}
+              {/* Image placeholder */}
             </div>
 
             <div className='col-md-8 text-dark bg-white p-3'>
               <div className='text-muted fs-section'>
-                {' '}
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                Quaerat alias, suscipit iusto nam culpa corporis possimus illo
-                quam est! Iusto deleniti provident accusamus asperiores fugiat,
-                nihil autem eveniet corporis eligendi dolor minus optio expedita
-                deserunt repellat! A, ipsa harum? Enim itaque nemo aliquam nam
-                harum illo porro suscipit accusamus culpa, error vero, nostrum
-                in iusto? Hic optio omnis rem laborum.
+                <h3>Impact Communautaire</h3>
+                <p>
+                  Chaque femme entrepreneure soutenue par SUMA crée un effet
+                  multiplicateur : elle emploie en moyenne 3 personnes, éduque
+                  ses enfants, et inspire d'autres femmes dans sa communauté.
+                  Notre modèle prouve que l'autonomisation financière des femmes
+                  transforme des vies et stimule le développement local.
+                </p>
+                <button className='btn btn-accent'>
+                  Voir nos réussites <ArrowRight />
+                </button>
               </div>
             </div>
           </div>
@@ -129,4 +119,5 @@ const Home = () => {
     </>
   )
 }
+
 export default Home
