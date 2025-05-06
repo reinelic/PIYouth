de22d9650c2138dd1ec765b3ba0fefaac84ba272
reinelic <reinelic@yahoo.fr>
2025-05-06 15:23:23 +0200

    changing copy to make it Suma

--- src/pages/Private/PrivateHome/PrivateHome.js
+++ src/pages/Private/PrivateHome/PrivateHome.js
@@ -4,21 +4,17 @@ const PrivateHome = () => {
   return (
     <div className='container'>
       <div className=' '>
-        <div className='row bg-white text-primary-100 p-2  mt-2'>
+        <div className='row text-primary-100 mt-2 bg-white  p-2'>
           <div className='col-md-5'>
             <img src='' alt='' class='img-thumbnail' />
-            <h5> Country Director du Burundi</h5>
           </div>
           <div className='col-md-7'>
-            <h5 className='text-muted'> Mot du Country Director</h5>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis
-            deleniti tenetur veritatis est facilis eius reprehenderit aut odit!
-            Fugit rerum nam vel provident? Excepturi ratione, libero possimus
-            perspiciatis voluptates odio est officiis aliquam, ullam laudantium
-            maxime dolorem velit nulla! Ex labore atque est, porro eum tenetur
-            cum id laboriosam quia animi deleniti, veniam pariatur
-            necessitatibus voluptatem blanditiis in provident dolorum iure
-            voluptatibus. Dolorum qui ab labore libero?
+            <h5 className='text-muted'> Mot de bienvenue </h5>
+            Chez SUMA, nous croyons que l'émancipation économique des femmes est
+            le pilier du développement communautaire. Notre mission est de vous
+            connecter aux institutions financières, de vous doter des
+            compétences nécessaires pour gérer et développer votre entreprise,
+            et de créer un réseau solidaire de femmes ambitieuses.
           </div>
         </div>
       </div>
