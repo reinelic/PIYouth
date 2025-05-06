b105b8f0e29b32a87a6196e6ee06ac6d5b90af59
reinelic <reinelic@yahoo.fr>
2025-05-06 15:05:48 +0200

    changing copy to make it Suma

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -50,7 +50,7 @@ const Home = () => {
 
               <div className='action_b'>
                 <Link to='/cdsList'>
-                  <span className='mr-2'>Réseaux de femmes</span>
+                  <span className='mr-2'>Réseaux</span>
                   <Hospital size={48} />
                 </Link>
               </div>
@@ -63,11 +63,11 @@ const Home = () => {
         <div className='container-fluid methodes padding-block-big'>
           <div className='row bg-body-primary'>
             <div className='col-md-7 contra'>
-              <img
+              {/* <img
                 src='/contra_component.png'
                 alt='Femmes entrepreneures'
                 className='component mb-2'
-              />
+              /> */}
               <div className='text-primary-100 w-70 fs-section mt-4'>
                 <h3>Notre Approche Holistique</h3>
                 <p>
